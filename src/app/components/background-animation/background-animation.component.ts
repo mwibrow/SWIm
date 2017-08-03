c9d80f4642bb996c29c0743293bc2ae4b407312e
Mark Wibrow <m.wibrow@gmail.com>
2017-08-03 10:28:57 +0100

    Sea as svg animation

--- src/app/components/background-animation/background-animation.component.ts
+++ src/app/components/background-animation/background-animation.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import "snapsvg-cjs";
 declare var Snap: any;
 
+const style = require('sass-extract-loader!./background-animation.component.scss');
 @Component({
   selector: 'app-background-animation',
   templateUrl: './background-animation.component.html',
@@ -13,29 +14,54 @@ export class BackgroundAnimationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
     this.createSvg();
+
+
   }
 
   createSvg() {
 
     let svgCanvas;
     svgCanvas = Snap("#svg");
+    let viewBox: any = svgCanvas.attr('viewBox');
 
-    var group = svgCanvas.group()
-    // Lets create big circle in the middle:
-    var bigCircle = svgCanvas.circle(150, 150, 100);
-    // By default its black, lets change its attributes
-    bigCircle.attr({
-      fill: "#bada55",
-      stroke: "#000",
-      strokeWidth: 5
+
+    let shape = svgCanvas.rect(viewBox.x, viewBox.y, viewBox.width, viewBox.height);
+    let color = style.global.$colorSky.value
+    shape.attr({
+      fill: `rgb(${color.r},${color.g},${color.b})`
     });
-    // Now lets create another small circle:
-    var smallCircle = group.circle(100, 150, 70);
 
-    var matrix = Snap.matrix(1, 0, 0, 1, 1900, 0);
-    group.append(bigCircle);
-    this.float(group);
+    let i: number;
+
+    shape = svgCanvas.rect(viewBox.x, viewBox.height / 3, viewBox.width, viewBox.height * 2 / 3);
+    color = style.global.$colorSea.value
+    shape.attr({
+      fill: `rgb(${color.r},${color.g},${color.b})`
+    });
+
+    let size = viewBox.height / 6;
+    let waveCount: number = 10;
+    for (i = 0;  i < waveCount ; i ++) {
+      shape = svgCanvas.ellipse(i * viewBox.width / (waveCount - 1) + 1, viewBox.height / 3, size, size * 1.0625);
+      color = style.global.$colorSky.value;
+      shape.attr({
+         fill: `rgb(${color.r},${color.g},${color.b})`
+      });
+      let bbox = shape.getBBox();
+      this.rotate(shape, Math.floor(Math.random() * 5) * 1000 + 6000);
+    }
+
+
+
+  }
+
+  rotate(shape, duration) {
+    let bbox = shape.getBBox();
+    shape.transform("r0," + bbox.cx + ',' + bbox.cy);
+    shape.animate({ transform: `r360,${bbox.cx},${bbox.cy}` }, duration,
+      () => {this.rotate(shape, duration)});
   }
 
   float(g) {
