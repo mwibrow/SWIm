d437435ce7a60d064ee307a2d533cb869402bfd8
Mark Wibrow <m.wibrow@gmail.com>
2017-08-03 15:16:02 +0100

    More work on snap-svg animation

--- src/app/components/background-animation/background-animation.component.ts
+++ src/app/components/background-animation/background-animation.component.ts
@@ -4,6 +4,8 @@ import "snapsvg-cjs";
 declare var Snap: any;
 
 const style = require('sass-extract-loader!./background-animation.component.scss');
+
+
 @Component({
   selector: 'app-background-animation',
   templateUrl: './background-animation.component.html',
@@ -14,7 +16,7 @@ export class BackgroundAnimationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-
+    console.log(style)
     this.createSvg();
 
 
@@ -53,8 +55,29 @@ export class BackgroundAnimationComponent implements OnInit {
       this.rotate(shape, Math.floor(Math.random() * 5) * 1000 + 6000);
     }
 
+    for (i = 0; i < 3; i ++) {
+      color = style.global.$colorClouds.value[2 - i].value
+      let g0 = svgCanvas.group().attr({
+        fill: `rgb(${color.r},${color.g},${color.b})`
+      });
+      let g1 = g0.group()
+      let g = g1.group()
+      let unit = viewBox.height / 8;
 
-
+      g.ellipse(48, 112, 48, 48);
+      g.ellipse(200, 104, 56, 56);
+      g.rect(48, 96, 152, 64);
+      g.ellipse(128, 64, 64, 64);
+      let m = new Snap.Matrix();
+      m.translate(0, viewBox.height / 4 - unit * i);
+      g.transform(m);
+      let bbox = g.getBBox();
+      m = new Snap.Matrix();
+      m.scale(1.5 - i / 4, 1.5 - i / 4, bbox.cx, bbox.cy );
+      g1.transform(m);
+      //g.transform('s' + (1 + i / 2));
+      this.float(g0, 60000 - i * 20000);
+    }
   }
 
   rotate(shape, duration) {
@@ -64,12 +87,12 @@ export class BackgroundAnimationComponent implements OnInit {
       () => {this.rotate(shape, duration)});
   }
 
-  float(g) {
+  float(g, speed) {
       var matrix = Snap.matrix(1, 0, 0, 1, -200, 0);
       g.transform(matrix);
       var matrix = Snap.matrix(1, 0, 0, 1, 1900, 0);
       var that = this;
-      g.animate({transform: matrix}, 20000, function() { that.float(g) });
+      g.animate({transform: matrix}, speed, function() { that.float(g, speed) });
   }
 }
 
