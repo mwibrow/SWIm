1653fd4587d1985c4b139d318b66fbdda79de065
Mark Wibrow <m.wibrow@gmail.com>
2017-09-30 17:50:44 +0100

    Timestamped folder includes seconds

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -123,8 +123,8 @@ export class TaskComponent implements OnInit {
 
   private runTask() {
     let now = new Date();
-    this.participantFolder = sprintf.sprintf('%04d%02d%02d-%02d%02d',
-      now.getFullYear(), now.getMonth() + 1, now.getDate(), now.getHours(), now.getMinutes());
+    this.participantFolder = sprintf.sprintf('%04d%02d%02d-%02d%02d%02d',
+      now.getFullYear(), now.getMonth() + 1, now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds());
     let participantPath = path.normalize(path.join(this.settings.responsesPath, this.participantFolder));
     fs.mkdirpSync(participantPath,
       (err) => {
